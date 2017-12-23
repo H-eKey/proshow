33d917bf0110946756945591c99b53469f32ee28
schekey <schekey@163.com>
2017-12-23 19:58:05 +0800

    1

---
+++ src/test/java/top/proshow/prolist/ProlistApplicationTests.java
@@ -0,0 +1,16 @@
+package top.proshow.prolist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProlistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
